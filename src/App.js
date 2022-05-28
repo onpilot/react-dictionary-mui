98b64c6b435b06465daf1f9866f801a1e699f110
Idan <72357530+onpilot@users.noreply.github.com>
2022-05-28 10:40:32 +0700

    add alert when word not found

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Container } from '@mui/material';
+import {
+  Alert,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material';
 import Header from './components/Header/Header';
 import Definitions from './components/Definitions/Definitions';
+import Footer from './components/Footer/Footer';
+import SocialLinks from './components/Footer/SocialLinks';
+import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher';
 
-const fetchDictionary = async (search, lang, setData) => {
+const fetchDictionary = async (
+  search,
+  lang,
+  setData,
+  setIsLoading,
+  setIsSearch,
+  setIsError
+) => {
   const url = 'https://api.dictionaryapi.dev/api/v2/entries';
-  const data = await axios.get(`${url}/${lang}/${search}`);
-  console.log(data.data);
-  setData(data.data);
+  try {
+    await setIsLoading(true);
+    await setIsError(false);
+    const data = await axios.get(`${url}/${lang}/${search}`);
+    await setData(data.data);
+    await setIsLoading(false);
+    await setIsSearch(false);
+  } catch (err) {
+    await setIsError(true);
+    await setIsLoading(false);
+    await setIsSearch(false);
+    console.error(err);
+  }
 };
 
 function App() {
@@ -16,25 +42,52 @@ function App() {
   const [isSearch, setIsSearch] = useState(false);
   const [lang, setLang] = useState('en');
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [darkmode, setDarkmode] = useState(true);
 
   useEffect(() => {
-    isSearch && fetchDictionary(search, lang, setData);
-    setIsSearch(false);
+    isSearch &&
+      fetchDictionary(
+        search,
+        lang,
+        setData,
+        setIsLoading,
+        setIsSearch,
+        setIsError
+      );
   }, [isSearch, search, lang]);
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: darkmode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div>
-      <Container maxWidth='md'>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container maxWidth='md' sx={{ margin: '0 auto 100px' }}>
+        <ThemeSwitcher darkmode={darkmode} setDarkmode={setDarkmode} />
         <Header
           search={search}
           setSearch={setSearch}
           lang={lang}
           setLang={setLang}
           setIsSearch={setIsSearch}
+          displayTooltip={data ? true : false}
         ></Header>
-        <Definitions data={data}></Definitions>
+        {isError ? (
+          <Alert severity='error'>
+            ERROR: No entry found! Try another word.
+          </Alert>
+        ) : (
+          <Definitions isLoading={isLoading} data={data}></Definitions>
+        )}
       </Container>
-    </div>
+      <SocialLinks />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
