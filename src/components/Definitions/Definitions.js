95b533f6339fcb6cd023665ad2ddb0beedf28507
Idan <72357530+onpilot@users.noreply.github.com>
2022-05-28 10:37:32 +0700

    add loading animation when data is being fetched

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,6 +1,7 @@
 import {
   Box,
   Chip,
+  CircularProgress,
   Divider,
   List,
   ListItem,
@@ -9,7 +10,7 @@ import {
   Typography,
 } from '@mui/material';
 
-const Definition = ({ data }) => {
+const Definitions = ({ isLoading, data }) => {
   if (!data) {
     return (
       <Box>
@@ -19,51 +20,59 @@ const Definition = ({ data }) => {
   }
   return (
     <Box sx={{ margin: '2rem 0' }}>
-      <Typography variant='body1'>Phonetics</Typography>
-      <List>
-        {data[0].phonetics ? (
-          data[0].phonetics.map((e, i) => (
-            <ListItem disablePadding key={`phonetic-${i}`}>
-              <ListItemButton>
-                {e.text ? (
-                  <ListItemText primary={e.text} />
-                ) : (
-                  <ListItemText>
-                    <s>no data available</s>
-                  </ListItemText>
-                )}
-                {e.audio && <audio src={e.audio} controls></audio>}
-              </ListItemButton>
-            </ListItem>
-          ))
-        ) : (
-          <Typography variant='body2'>data unavailable</Typography>
-        )}
-      </List>
-      <Divider sx={{ margin: '1rem 0' }} />
-      <Typography variant='body1'>Definitions</Typography>
-      <List>
-        {data.map((d) =>
-          d.meanings.map((e, i) => (
-            <ListItemButton key={i}>
-              {
-                <Box>
-                  <Typography variant='body2'>{e.partOfSpeech}</Typography>
-                  {e.definitions.map((def, i) => (
-                    <ListItem disablePadding key={`definition-${i}`}>
-                      <ListItemButton>
-                        <ListItemText primary={def.definition} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
-                </Box>
-              }
-            </ListItemButton>
-          ))
-        )}
-      </List>
+      {isLoading ? (
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box>
+          <Typography variant='body1'>Phonetics</Typography>
+          <List>
+            {data[0].phonetics ? (
+              data[0].phonetics.map((e, i) => (
+                <ListItem disablePadding key={`phonetic-${i}`}>
+                  <ListItemButton>
+                    {e.text ? (
+                      <ListItemText primary={e.text} />
+                    ) : (
+                      <ListItemText>
+                        <s>no data available</s>
+                      </ListItemText>
+                    )}
+                    {e.audio && <audio src={e.audio} controls></audio>}
+                  </ListItemButton>
+                </ListItem>
+              ))
+            ) : (
+              <Typography variant='body2'>data unavailable</Typography>
+            )}
+          </List>
+          <Divider sx={{ margin: '1rem 0' }} />
+          <Typography variant='body1'>Definitions</Typography>
+          <List>
+            {data.map((d) =>
+              d.meanings.map((e, i) => (
+                <ListItemButton key={i}>
+                  {
+                    <Box>
+                      <Typography variant='body2'>{e.partOfSpeech}</Typography>
+                      {e.definitions.map((def, i) => (
+                        <ListItem disablePadding key={`definition-${i}`}>
+                          <ListItemButton>
+                            <ListItemText primary={def.definition} />
+                          </ListItemButton>
+                        </ListItem>
+                      ))}
+                    </Box>
+                  }
+                </ListItemButton>
+              ))
+            )}
+          </List>
+        </Box>
+      )}
     </Box>
   );
 };
 
-export default Definition;
+export default Definitions;
